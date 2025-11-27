be5b821cbdf7d034dba80f190a52962af3d4f5d7
anirudha <anirudhasj441@gmail.com>
2025-11-27 16:59:51 +0000

    fix: use custom user in authenticat views

--- web/src/authentication/views.py
+++ web/src/authentication/views.py
@@ -18,7 +18,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
+
+User = get_user_model()
 
 def getUserToken(user):
     """
